a77a405e09691a0dd11a410369e264ceb847e5da
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-07 21:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = Object.assign({}, driver);
   
   newDriver[key] = value;
   
